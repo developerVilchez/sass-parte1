b464999d9e547660fe0e69e6bd4416a6b73a101a
lulublondet <maria.mlvp@gmail.com>
2017-06-27 14:04:46 -0500

    consumiendo datos del api

---
+++ assets/js/index.js
@@ -0,0 +1,38 @@
+const get = url => {
+  return new Promise((resolve, reject) => {
+    var xhr = new XMLHttpRequest();
+    xhr.addEventListener('load', _ => {
+      if (xhr.status !== 200) {
+        reject(new Error(xhr.statusText));
+      }
+      resolve(xhr.response);
+    });
+    
+    xhr.open('GET', url);
+    xhr.responseType = 'json';
+    xhr.send();
+  })
+}
+
+
+const Laboratoria = {
+  alumna : null
+}
+
+
+$( _ => {
+  get('http://laboratoria.cuadra.co:9339/api/v1/students/').then(response => {
+    console.log("Success! Estableciste conexion");
+    Laboratoria.alumna =  response;
+    for(var i in Laboratoria.alumna){
+        $("ol").append('<li class="alumna"></li>');
+    }
+    jQuery.each(Laboratoria.alumna, function(i,val){
+      $('li.alumna').eq(i).text(val.name + " " + val.pLastName + " " +  val.mLastName);
+    })
+
+  }).catch(error => {
+      console.error("Failed! No se estableció conexíón", error);
+      });
+
+});
